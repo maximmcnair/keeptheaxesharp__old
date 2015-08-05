927a5c79ad395bb6c8472f7467346d8d476ac16b
Maxim McNair <mail@maximmcnair.com>
2015-08-05 19:49:53 +0100

    add babel to gulp mocha

--- gulpfile.js
+++ gulpfile.js
@@ -33,10 +33,16 @@ gulp.task('browserify', function () {
  * Javascript Testing
  */
 var mocha = require('gulp-mocha');
+var babel = require('babel/register');
 gulp.task('js:test', function () {
-  return gulp.src('test.js', {read: false})
+  return gulp.src('./test/testSpec.js', {read: false})
     // gulp-mocha needs filepaths so you can't have any plugins before it
-    .pipe(mocha({reporter: 'nyan'}));
+    .pipe(mocha({
+      reporter: 'nyan',
+      compilers: {
+        js: babel
+      }
+    }));
 });
 
 /*
