4b14fda367907164ac5b6749182189e37116e51e
Maxim McNair <mail@maximmcnair.com>
2015-08-05 18:44:25 +0100

    setup js build system

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,24 @@
 
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
+var browserify = require('gulp-browserify');
+/* eslint-disable no-unused-vars */
+var babelify = require('babelify');
+/* eslint-enable no-unused-vars */
 
 gulp.task('copy_index', function () {
   gulp.src('src/*.html').pipe(gulp.dest('public/'));
 });
-gulp.task('default', ['copy_index']);
+
+gulp.task('browserify', function () {
+  return gulp.src('./src/js/app.js', {entry: true})
+    .pipe(browserify({
+      transform: ['babelify']
+    }))
+    .pipe(gulp.dest('public/assets/js/'));
+});
+
+gulp.task('default', ['copy_index', 'browserify']);
 
 gulp.task('compiled', ['default']);
 
