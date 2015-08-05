8aa7c178e0e46933258df60af0ff09812ee08118
Maxim McNair <mail@maximmcnair.com>
2015-08-05 19:52:56 +0100

    remove unneeded comments form gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,6 @@ var mocha = require('gulp-mocha');
 var babel = require('babel/register');
 gulp.task('js:test', function () {
   return gulp.src('./test/testSpec.js', {read: false})
-    // gulp-mocha needs filepaths so you can't have any plugins before it
     .pipe(mocha({
       reporter: 'nyan',
       compilers: {
