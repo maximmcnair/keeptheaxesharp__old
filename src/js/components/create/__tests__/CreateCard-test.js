50795301a869a1f1fec10959e4be8fa5a5381231
Maxim McNair <mail@maximmcnair.com>
2015-08-21 13:08:22 +0100

    add createCard test

---
+++ src/js/components/create/__tests__/CreateCard-test.js
@@ -0,0 +1,19 @@
+/**
+ * Create Card Component Spec
+ */
+
+/**
+ * @test {CreateCardComponent}
+ */
+describe('CreateCardComponent', () => {
+  var CreateCard;
+  var React = require('react');
+  var TestUtils = require('react/lib/ReactTestUtils');
+
+  beforeEach(() => {
+    CreateCardComponent = require('../CreateCardComponent.js');
+  });
+
+  it('');
+
+});
